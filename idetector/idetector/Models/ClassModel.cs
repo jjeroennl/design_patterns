500e21847f4f1038ea8eaa3b89613bf9051721f5
Martijn Sikkema <martijn.sikkema@knmi.nl>
2019-11-27 15:47:19 +0100

    Unit Tests for PropertyModel
    Added unit tests for PropertyModel that covers its construction.
    Changed access modifiers of all models to public.

--- idetector/idetector/Models/ClassModel.cs
+++ idetector/idetector/Models/ClassModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace idetector.Models
 {
-    class ClassModel
+    public class ClassModel
     {
     }
 }
--- idetector/idetector/Models/PropertyModel.cs
+++ idetector/idetector/Models/PropertyModel.cs
@@ -7,22 +7,21 @@ using System.Threading.Tasks;
 
 namespace idetector.Models
 {
-    class PropertyModel
+    public class PropertyModel
     {
         private PropertyDeclarationSyntax _node { get; set; }
         public ClassModel Parent { get; set; }
-        public string[] Modifier { get; set; }
+        public string[] Modifiers { get; set; }
         public string Type { get; set; }
         public string Identifier { get; set; }
 
         public PropertyModel(PropertyDeclarationSyntax node)
         {
             _node = node;
-
-            Modifier = new string[_node.Modifiers.Count];
+            Modifiers = new string[_node.Modifiers.Count];
             for (int i = 0; i < _node.Modifiers.Count; i++)
             {
-                Modifier[i] = _node.Modifiers[i].ToString();
+                Modifiers[i] = _node.Modifiers[i].ToString();
             }
 
             Type = _node.Type.ToString();
