5d67d6b280cad56740636bbcec439b6a1a37aca9
martschilt <mart.schilthuis@knmi.nl>
2019-11-28 12:07:16 +0100

    Made xUnitTest file

--- idetector/idetector/Models/ClassModel.cs
+++ idetector/idetector/Models/ClassModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace idetector.Models
 {
     public class ClassModel
     {
+        public List<MethodModel> methods = new List<MethodModel>();
+        public List<PropertyModel> properties = new List<PropertyModel>();
+
+        public ClassModel(ClassDeclarationSyntax node)
+        {
+
+        }
     }
 }
---
+++ idetector/xUnitTest/ClassModelTests.cs
@@ -0,0 +1,50 @@
+﻿using idetector.Models;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace xUnitTest
+{
+    public class ClassModelTests
+    {
+        public SyntaxTree setup()
+        {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(@"using System;
+            using System.Collections;
+            using System.Linq;
+            using System.Text;
+ 
+            namespace HelloWorld
+            {
+                class Program : Controller
+                {
+                    static void Main(string[] args , string foo)
+                    {
+Console.WriteLine('Hello, World!');
+}
+                    public void Foo(){
+                        return""Bar"";
+                    }
+                }
+            }");
+            return tree;
+        }
+
+        [Fact]
+        public void Test_Construction()
+        {
+            var tree = setup();
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+            MemberDeclarationSyntax firstMember = root.Members[0];
+            NamespaceDeclarationSyntax namespaceDeclaration = (NamespaceDeclarationSyntax)firstMember;
+            ClassDeclarationSyntax classDeclaration = (ClassDeclarationSyntax)namespaceDeclaration.Members[0];
+
+            ClassModel classModel = new ClassModel(classDeclaration);
+
+            Assert.Equal(2, classModel.methods.Count());
+        }
+    }
+}
