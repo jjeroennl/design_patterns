53983be043baddc8f06b1bb5e65cb323b3748e5e
martschilt <mart.schilthuis@knmi.nl>
2019-12-02 15:40:02 +0100

    ASCII table added

--- idetector/idetector/Program.cs
+++ idetector/idetector/Program.cs
@@ -19,11 +19,20 @@ namespace idetector
     {
         static async Task Main(string[] args)
         {
+            string title;
+            int percentage;
+
             Console.WriteLine(@" _     _      _            _             
 (_) __| | ___| |_ ___  ___| |_ ___  _ __ 
 | |/ _` |/ _ \ __/ _ \/ __| __/ _ \| '__|
 | | (_| |  __/ ||  __/ (__| || (_) | |   
 |_|\__,_|\___|\__\___|\___|\__\___/|_|");
+            StringBuilder sb = new StringBuilder("", 2);
+            sb.Append(@"
+                 RESULTS
++------------------+-------------+------+
+|     Pattern      | Probability | Info |
++------------------+-------------+------+");
 
             SyntaxTree tree = CSharpSyntaxTree.ParseText(@"using System;
             using System.Collections;
@@ -55,11 +64,23 @@ namespace idetector
                 }
             }");
 
+            /*
             ClassWalker w = new ClassWalker();
             w.Visit(tree.GetRoot());
             
             Walker w2 = new Walker();
-            w2.Visit(tree.GetRoot());
+            w2.Visit(tree.GetRoot()); 
+            */
+
+            // Singleton
+            title = "Singleton";
+            percentage = 80;
+            sb.Append(@"
+| " + title.PadRight(16) + " | " + percentage.ToString().PadRight(3) + " %       | Link |");
+
+            sb.Append(@"
++------------------+-------------+------+");
+            Console.WriteLine(sb.ToString());
         }
     }
 }
