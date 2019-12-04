20dccc99e3da96af8af05bf152fa4b70684a03bc
Martijn Sikkema <martijn.sikkema@knmi.nl>
2019-12-04 12:11:09 +0100

    Created initial PriorityCollection class

---
+++ idetector/idetector/Collections/PriorityCollection.cs
@@ -0,0 +1,45 @@
+﻿using idetector.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace idetector.Collections
+{
+    class PriorityCollection
+    {
+        /// <summary>
+        /// Tkey as Namespace/Classname
+        /// </summary>
+        private static Dictionary<string, List<Priority>> priorities = new Dictionary<string, List<Priority>>();
+
+        public static void AddPriority(string pattern, Priority priority)
+        {
+            if (priorities.ContainsKey(pattern))
+            {
+                priorities[pattern].Add(priority);
+            }
+            else
+            {
+                priorities.Add(pattern, new List<Priority>());
+                priorities[pattern].Add(priority);
+            }
+        }
+
+        public static List<Priority> GetPriorities(string pattern)
+        {
+            try
+            {
+                return priorities[pattern];
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public static Dictionary<string, List<Priority>> GetPriorities()
+        {
+            return priorities;
+        }
+    }
+}
---
+++ idetector/xUnitTest/PriorityCollectionTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace xUnitTest
+{
+    public class PriorityCollectionTests
+    {
+
+        [Fact]
+        public void Test_AddPriorityToCollection()
+        {
+
+        }
+
+    }
+}
