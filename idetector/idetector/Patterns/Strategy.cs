e9c7f103855c6b65c1b75fe661fbb5bf1530ecac
martschilt <mart.schilthuis@knmi.nl>
2019-12-09 10:45:34 +0100

    strategy class added

---
+++ idetector/idetector/Patterns/Strategy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using idetector.Collections;
+using idetector.Models;
+
+namespace idetector.Patterns
+{
+    public class Strategy : IPattern
+    {
+        private int _score;
+        private ClassModel cls;
+
+        public Strategy(ClassModel _cls)
+        {
+            cls = _cls;
+            PriorityCollection.ClearPriorities();
+            PriorityCollection.AddPriority("strategy", "IsPrivateConstructor", Priority.Low);
+            PriorityCollection.AddPriority("strategy", "IsStaticSelf", Priority.Low);
+            PriorityCollection.AddPriority("strategy", "IsGetInstance", Priority.Low);
+            PriorityCollection.AddPriority("strategy", "IsCreateSelf", Priority.Low);
+        }
+        public void Scan()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ idetector/xUnitTest/StrategyTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using idetector.Models;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Linq;
+using idetector.Collections;
+using idetector.Parser;
+using idetector.Patterns;
+using Xunit;
+
+namespace xUnitTest
+{
+    public class StrategyTests
+    {
+        SyntaxTree Successsetup()
+        {
+            return CSharpSyntaxTree.ParseText(@" 
+               class User{
+                    private static User me;
+
+                    private User(){
+
+                    }
+                    
+                    public static User getUser(){
+                        if(this.me == null){
+                            this.me = new User();
+                        }
+                        
+                        return this.me;
+                    }
+                }");
+        }
+        [Fact]
+        public void Test_Strategy_Succeed()
+        {
+            var tree = Successsetup();
+            Walker.GenerateModels(tree);
+
+            Strategy strategy = new Strategy(ClassCollection.GetClass("User"));
+            strategy.Scan();
+            Assert.Equal(100, strategy.Score());
+        }
+    }
+}
