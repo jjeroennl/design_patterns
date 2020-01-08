88e52c7017acbd9a451ce28dc12ddf7ff7bc90c5
sybr0 <scrubblsz@gmail.com>
2020-01-09 00:42:22 +0100

    added new checks removed old checks

--- idetector/idetector/Patterns/Observer.cs
+++ idetector/idetector/Patterns/Observer.cs
@@ -1,12 +1,14 @@
 ﻿using idetector.Collections;
 using idetector.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Type = System.Type;
 
 namespace idetector.Patterns
 {
@@ -14,9 +16,8 @@ namespace idetector.Patterns
     {
 
         private float _score;
-        private Dictionary<ClassModel, int> _scores = new Dictionary<ClassModel, int>();
-        private ClassCollection cc;
-        private List<ClassModel> interfaces = new List<ClassModel>();
+        private readonly ClassCollection cc;
+        private List<string> interfaces = new List<string>();
         private List<RequirementResult> _results = new List<RequirementResult>();
 
 
@@ -27,19 +28,11 @@ namespace idetector.Patterns
 
         public void Scan()
         {
-            SetInterfaces();
-
             _score = 0;
+            _results.Clear();
+            _results.Add(HasObserverInterface());
+            _results.Add(HasObserverRelations());
 
-            // doet bijna hetzelfde
-            // 1 checkt of er een interface is met een void function genaamd update die een interface als parameter heeft
-            // 2 checkt of er een interface is met een void function die een interface als parameter heeft
-            _results.Add(HasObserverInterfaceWithUpdateFunction());
-            _results.Add(HasObserverInterfaceWithNamedUpdateFunction());
-
-            _results.Add(HasSubjectWithObserverList());
-            _results.Add(HasSubjectFunctions());
-            _results.Add(ConcreteObserverExtendsIObserver());
 
         }
 
@@ -48,25 +41,13 @@ namespace idetector.Patterns
             return _results;
         }
 
-        private void SetInterfaces()
+        public float Score()
         {
-            foreach (KeyValuePair<string, ClassModel> cls in cc.GetClasses())
-            {
-                if (cls.Value.IsInterface)
-                {
-                    interfaces.Add(cls.Value);
-                }
-            }
+            return _score;
         }
 
-        public int Score(ClassModel clsModel)
-        {
-            if (this._scores.ContainsKey(clsModel)) return this._scores[clsModel];
-            return 0;
-        }
-
-        // Checks for existing interface with a void function that's named 'update' and has an interface as parameter
-        public RequirementResult HasObserverInterfaceWithNamedUpdateFunction()
+        // Checks if there is an observer interface
+        public RequirementResult HasObserverInterface()
         {
             foreach (KeyValuePair<string, ClassModel> cls in cc.GetClasses())
             {
@@ -74,152 +55,44 @@ namespace idetector.Patterns
                 {
                     if (cls.Value.IsInterface)
                     {
-                        foreach (var method in cls.Value.getMethods())
-                        {
-                            if (method.ReturnType.ToLower().Equals("void") && method.Identifier.ToLower().Equals("update"))
-                            {
-                                // checks if parameter is an interface
-                                string parameters = method.Parameters.Replace("(", string.Empty).Replace(")", string.Empty);
-                                string[] paramList = parameters.Split(" ");
-                                ClassModel targetClass = cc.GetClass(paramList[0]);
-                                if (targetClass.IsInterface)
-                                {
-                                    return new RequirementResult("OBSERVER-HAS-INTERFACE-WITH-NAMED-UPDATE-FUNCTION", true);
-                                }
-                            }
-                        }
+                        // check of naam IObserver is?
+                        // check of de interface een void functie heeft die een interface als parameter heeft?
+                        // check of de naam van bovenstaande functie update is? @justin
+
+                        return new RequirementResult("OBSERVER-HAS-OBSERVER-INTERFACE", true);
                     }
                 }
             }
-            return new RequirementResult("OBSERVER-HAS-INTERFACE-WITH-NAMED-UPDATE-FUNCTION", false);
+            return new RequirementResult("OBSERVER-HAS-OBSERVER-INTERFACE", false);
         }
 
-        // Checks for existing interface with a void function that has an interface as parameter
-        public RequirementResult HasObserverInterfaceWithUpdateFunction()
+        // Checks if there are relations between the subject and the observer(s)
+        public RequirementResult HasObserverRelations()
         {
-            foreach (KeyValuePair<string, ClassModel> cls in cc.GetClasses())
-            {
-                if (cls.Value.Modifiers != null)
-                {
-                    if (cls.Value.IsInterface)
-                    {
-                        if (cls.Value.getMethods().Count > 1)
-                        {
-                            return new RequirementResult("OBSERVER-HAS-INTERFACE-WITH-UPDATE-FUNCTION", false);
-                        }
-                        else
-                        {
-                            foreach (var method in cls.Value.getMethods())
-                            {
-                                if (method.ReturnType.ToLower().Equals("void"))
-                                {
-                                    // checks if parameter is an interface
-                                    string parameters = method.Parameters.Replace("(", string.Empty).Replace(")", string.Empty);
-                                    string[] paramList = parameters.Split(" ");
-                                    ClassModel targetClass = cc.GetClass(paramList[0]);
-                                    if (targetClass.IsInterface)
-                                    {
-                                        return new RequirementResult("OBSERVER-HAS-INTERFACE-WITH-UPDATE-FUNCTION", true);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            return new RequirementResult("OBSERVER-HAS-INTERFACE-WITH-UPDATE-FUNCTION", false);
-        }
-        
-        // Checks for a subject class that has at least 2 void functions
-        // that could indicate an observer pattern (subscribe & unsubscribe)
-        public RequirementResult HasSubjectFunctions()
-        {
-            int probability = 0;
-            foreach (KeyValuePair<string, ClassModel> cls in cc.GetClasses())
-            {
-                if (cls.Value.Modifiers != null)
-                {
-                    foreach (var method in cls.Value.getMethods())
-                    {
-                        // does it have to be public idk
-                        if (method.Modifiers.Contains("public") && method.ReturnType.ToLower().Equals("void"))
-                        {
-                            // checks if parameter is an interface
-                            string parameters = method.Parameters.Replace("(", string.Empty).Replace(")", string.Empty);
-                            string[] paramList = parameters.Split(" ");
-                            ClassModel targetClass = cc.GetClass(paramList[0]);
-                            // Checks if parameter is an observer interface or abstract class
-                            if (targetClass.IsInterface || (targetClass.Modifiers.Contains("abstract")) )
-                            {
-                                probability++;
-                                if (probability == 2)
-                                {
-                                    return new RequirementResult("OBSERVER-HAS-SUBJECT-FUNCTIONS", true);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            return new RequirementResult("OBSERVER-HAS-SUBJECT-FUNCTIONS", false);
-        }
-
-        // Checks if there is a class that has a list of interfaces. 
-        // If true, probably a subject class with a list filled with observers
-        public RequirementResult HasSubjectWithObserverList()
-        {
-            foreach (KeyValuePair<string, ClassModel> cls in cc.GetClasses())
-            {
-                if (cls.Value.Modifiers != null)
-                {
-                    foreach (var property in cls.Value.getProperties())
-                    {
-                        string identifier = property.ValueType;
-                        // Usually observers are stored in some kind of collection, most often in a list (no checks for other collection types for now)
-                        if (identifier.ToLower().Contains("list"))
-                        {
-                            int pFrom = identifier.IndexOf("<") + "<".Length;
-                            int pTo = identifier.LastIndexOf(">");
-
-                            string target = identifier.Substring(pFrom, pTo - pFrom);
-
-                            ClassModel targetClass = cc.GetClass("IObserver");
-
-                            // not sure if it has to be a list of interfaces or a list of classes that extend an interface but
-                            // example code had a list of interfaces 
-                            return new RequirementResult("OBSERVER-HAS-SUBJECT-WITH-OBSERVER-LIST", true);
-                            }
-                    }
-                }
-            }
-
-            return new RequirementResult("OBSERVER-HAS-SUBJECT-WITH-OBSERVER-LIST", false);
-        }
-
-        // Checks if there is a class that extends 'IObserver'
-        // names can be different though so idk if makes sense to check at all
-        public RequirementResult ConcreteObserverExtendsIObserver()
-        { 
             foreach (KeyValuePair<string, ClassModel> cls in cc.GetClasses())
             {
                 List<string> parents = cls.Value.GetParents();
-                if (parents != null)
-                {
-                    // ervan uitgaande dat observer interface altijd IObserver heet
-                    // misschien aanpassen naar het checken van interface implemention en dan kijken of die interface weer voldoet aan
-                    // de observer interface requirements ofzo
-                    if (parents.Contains("IObserver"))
+
+
+                    // get all classes that extend the observer interface
+                    // API: get all interfaces from tree
+                    if (parents != null && parents.Contains("IObserver"))
                     {
-                        ClassModel target = cc.GetClass(parents[0]);
-                        if (target.IsInterface)
-                        {
-                            return new RequirementResult("OBSERVER-CONCRETE-OBSERVER-EXTENDS-IOBSERVERS", true);
-                        }
+                        interfaces.Add(cls.Value.Identifier);
                     }
-                }
             }
 
-            return new RequirementResult("OBSERVER-CONCRETE-OBSERVER-EXTENDS-IOBSERVERS", false);
+            // Op zoek naar collection
+            // Op zoek of er een object is die een IEnumerable is
+            // Checken of die IEnumerable alle interfaces die we hebben gevonden bevat
+            // API: check if type of checked attribute is IEnumerable
+
+            if (interfaces.Count > 0)
+            {
+                return new RequirementResult("OBSERVER-HAS-OBSERVER-RELATIONS", true);
+            }
+            return new RequirementResult("OBSERVER-HAS-OBSERVER-RELATIONS", false);
         }
+        
     }
 }
--- idetector/xUnitTest/ObserverTests.cs
+++ idetector/xUnitTest/ObserverTests.cs
@@ -8,231 +8,142 @@ using Xunit;
 
 namespace xUnitTest
 {
-    // The sole responsibility of a subject is to maintain a list of observers 
-    // and to notify them of state changes by calling their update() operation.
 
-    // Check that subject loops through list of observers and calls a known function
-
-    // The responsibility of observers is to register (and unregister) themselves on a subject (to get notified of state changes) 
-    // and to update their state (synchronize their state with subject's state) when they are notified.
-
-    // Observer interface has Update function
-    // ConcreteObservers inherit update func from Observer interface
-
-    // Subject Interface usually has 3 functions:
-    // Subscribe (Register), unsubscribe (unregister) and notify 
-
-    // (Concrete)Subject has a list or similar 
-    // containing observers that subject is subscribed to
-
-    // (Concrete)Subject sub/unsub have same parameters
+    // Before running any checks, check if there is an observer interface. if not -> stop checking
+    // Get all classes that extend the observer interface
     public class ObserverTests
     {
-
-        SyntaxTree OobserverTree()
-        {
-            return CSharpSyntaxTree.ParseText(@" 
-                public interface IObserver
-                {
-                    void Update(ISubject subject);
-                }
-
-                public interface ISubject
-                {
-                    void Attach(IObserver observer);
-                    void Detach(IObserver observer);
-                    void Notify();
-                }
-
-                public class Subject : ISubject
-                {
-
-                    public int State { get; set; } = -0;
-                    private List<IObserver> _observers = new List<IObserver>();
-
-                    public void Attach(IObserver observer)
-                    {
-                        this._observers.Add(observer);
-                    }
-
-                    public void Detach(IObserver observer)
-                    {
-                        this._observers.Remove(observer);
-                    }
-
-                    public void Notify()
-                    {
-                        foreach (var observer in _observers)
-                        {
-                            observer.Update(this);
-                        }
-                    }
-
-                    public void SomeBusinessLogic()
-                    {
-                        this.State = new Random().Next(0, 10);
-                        Thread.Sleep(15);
-                        this.Notify();
-                    }
-                }
-
-                class ConcreteObserverA : IObserver
-                {
-                    public void Update(ISubject subject)
-                    {
-                        if ((subject as Subject).State < 3)
-                        {
-                            Console.WriteLine('ConcreteObserverA: Reacted to the event.');
-                        }
-                    }
-                }
-
-                class ConcreteObserverB : IObserver
-                {
-                    public void Update(ISubject subject)
-                    {
-                        if ((subject as Subject).State == 0 || (subject as Subject).State >= 2)
-                        {
-                            Console.WriteLine('ConcreteObserverB: Reacted to the event.');
-                        }
-                    }
-                }");
-        }
 
         SyntaxTree ObserverTree()
         {
             return CSharpSyntaxTree.ParseText(@" 
+        using System;
+        using System.Collections.Generic;
+        using System.Threading;
 
-    /// <summary>
-
-    /// Entry point into console application.
-
-    /// </summary>
-
-    static void Main()
-    {
-      // Configure Observer pattern
-
-      ConcreteSubject s = new ConcreteSubject();
- 
-      s.Attach(new ConcreteObserver(s, 'x'));
-      s.Attach(new ConcreteObserver(s, 'y'));
-            s.Attach(new ConcreteObserver(s, 'z'));
-
-            // Change subject and notify observers
-
-            s.SubjectState = 'abc';
-            s.Notify();
-
-            // Wait for user
-
-            Console.ReadKey();
-        }
-    }
-
-    /// <summary>
-
-    /// The 'Subject' abstract class
-
-    /// </summary>
-
-    abstract class Subject
-
-    {
-        private List<IObserver> _observers = new List<IObserver>();
-
-        public void Attach(Observer observer)
+        namespace RefactoringGuru.DesignPatterns.Observer.Conceptual
         {
-            _observers.Add(observer);
-        }
-
-        public void Detach(Observer observer)
-        {
-            _observers.Remove(observer);
-        }
-
-        public void Notify()
-        {
-            foreach (Observer o in _observers)
+            public interface IObserver
             {
-                o.Update();
+                void Update(ISubject subject);
             }
+
+            public interface ISubject
+            {
+                void Attach(IObserver observer);
+
+                void Detach(IObserver observer);
+
+                void Notify();
+            }
+
+            public class Subject : ISubject
+            {
+                public int State { get; set; } = -0;
+
+                private List<IObserver> _observers = new List<IObserver>();
+
+                public void Attach(IObserver observer)
+                {
+                    Console.WriteLine('Subject: Attached an observer.');
+                    this._observers.Add(observer);
+                }
+
+                public void Detach(IObserver observer)
+                {
+                    this._observers.Remove(observer);
+                    Console.WriteLine('Subject: Detached an observer.');
+                }
+
+                public void Notify()
+                {
+                    Console.WriteLine('Subject: Notifying observers...');
+
+                    foreach (var observer in _observers)
+                    {
+                        observer.Update(this);
+                    }
+                }
+
+                public void SomeBusinessLogic()
+                {
+                    Console.WriteLine('\nSubject: I'm doing something important.');
+                    this.State = new Random().Next(0, 10);
+
+                    Thread.Sleep(15);
+
+                    Console.WriteLine('Subject: My state has just changed to: ' + this.State);
+                    this.Notify();
+                }
+            }
+
+
+            class ConcreteObserverA : IObserver
+            {
+                public void Update(ISubject subject)
+                {            
+                    if ((subject as Subject).State < 3)
+                    {
+                        Console.WriteLine('ConcreteObserverA: Reacted to the event.');
+                    }
+                }
+            }
+
+            class ConcreteObserverB : IObserver
+            {
+                public void Update(ISubject subject)
+                {
+                    if ((subject as Subject).State == 0 || (subject as Subject).State >= 2)
+                    {
+                        Console.WriteLine('ConcreteObserverB: Reacted to the event.');
+                    }
+                }
+            }
+
+            class ConcreteObserverC : IObserver
+            {
+                public void Update(ISubject subject)
+                {
+                    if ((subject as Subject).State == 0 || (subject as Subject).State >= 2)
+                    {
+                        Console.WriteLine('ConcreteObserverC: Reacted to the event.');
+                    }
+                }
+            }
+
+            class ConcreteObserverD : IObserver
+            {
+                public void Update(ISubject subject)
+                {
+                    if ((subject as Subject).State == 0 || (subject as Subject).State >= 2)
+                    {
+                        Console.WriteLine('ConcreteObserverD: Reacted to the event.');
+                    }
+                }
+            }
+            
+            class Program
+            {
+                static void Main(string[] args)
+                {
+                    // The client code.
+                    var subject = new Subject();
+                    var observerA = new ConcreteObserverA();
+                    subject.Attach(observerA);
+
+                    var observerB = new ConcreteObserverB();
+                    subject.Attach(observerB);
+
+                    subject.SomeBusinessLogic();
+                    subject.SomeBusinessLogic();
+
+                    subject.Detach(observerB);
+
+                    subject.SomeBusinessLogic();
+                }
+            }
+        }");
         }
-    }
-
-    /// <summary>
-
-    /// The 'ConcreteSubject' class
-
-    /// </summary>
-
-    class ConcreteSubject : Subject
-
-    {
-        private string _subjectState;
-
-        // Gets or sets subject state
-
-        public string SubjectState
-        {
-            get { return _subjectState; }
-            set { _subjectState = value; }
-        }
-    }
-
-    /// <summary>
-
-    /// The 'Observer' abstract class
-
-    /// </summary>
-
-    abstract class Observer
-
-    {
-        public abstract void Update();
-    }
-
-    /// <summary>
-
-    /// The 'ConcreteObserver' class
-
-    /// </summary>
-
-    class ConcreteObserver : Observer
-
-    {
-        private string _name;
-        private string _observerState;
-        private ConcreteSubject _subject;
-
-        // Constructor
-
-        public ConcreteObserver(
-          ConcreteSubject subject, string name)
-        {
-            this._subject = subject;
-            this._name = name;
-        }
-
-        public override void Update()
-        {
-            _observerState = _subject.SubjectState;
-            Console.WriteLine('Observer {0}'s new state is {1}',
-              _name, _observerState);
-        }
-
-        // Gets or sets subject
-
-        public ConcreteSubject Subject
-        {
-            get { return _subject; }
-            set { _subject = value; }
-        }
-    ");
-        }
-
-
-
         SyntaxTree NoObserverList()
         {
             return CSharpSyntaxTree.ParseText(@" 
@@ -360,107 +271,24 @@ namespace xUnitTest
         }
 
         [Fact]
-        public void Test_Observer_HasObserverInterfaceWithUpdateFunction()
-        {
-            Requirements r = new Requirements();
-            ScoreCalculator calculator = new ScoreCalculator(r.GetRequirements());
-            var tree = ObserverTree();
-            var collection = Walker.GenerateModels(tree);
-
-            Observer observer = new Observer(collection);
-            var score = calculator.GetScore("OBSERVER", observer.GetResult());
-            Assert.Equal(100, score);
-        }
-
-        [Fact]
         public void Test_Observer_HasObserverInterface()
         {
-            Requirements r = new Requirements();
-            ScoreCalculator calculator = new ScoreCalculator(r.GetRequirements());
             var tree = ObserverTree();
             var collection = Walker.GenerateModels(tree);
 
             Observer observer = new Observer(collection);
-            var score = calculator.GetScore("OBSERVER", observer.GetResult());
-            Assert.Equal(100, score);
+            Assert.True(observer.HasObserverInterface().Passed);
         }
 
         [Fact]
-        public void Test_Observer_HasSubjectFunctions()
+        public void Test_Observer_HasObserverRelations()
         {
-            Requirements r = new Requirements();
-            ScoreCalculator calculator = new ScoreCalculator(r.GetRequirements());
             var tree = ObserverTree();
             var collection = Walker.GenerateModels(tree);
 
             Observer observer = new Observer(collection);
-            var score = calculator.GetScore("OBSERVER", observer.GetResult());
-            Assert.Equal(100, score);
+            Assert.True(observer.HasObserverRelations().Passed);
         }
 
-        [Fact]
-        public void Test_Observer_HasSubjectWithObserverList()
-        {
-            Requirements r = new Requirements();
-            ScoreCalculator calculator = new ScoreCalculator(r.GetRequirements());
-            var tree = ObserverTree();
-            var collection = Walker.GenerateModels(tree);
-
-            Observer observer = new Observer(collection);
-            var score = calculator.GetScore("OBSERVER", observer.GetResult());
-            Assert.Equal(100, score);
-        }
-
-        [Fact]
-        public void Test_Observer_ConcreteObserverExtendsIObserver()
-        {
-            Requirements r = new Requirements();
-            ScoreCalculator calculator = new ScoreCalculator(r.GetRequirements());
-            var tree = ObserverTree();
-            var collection = Walker.GenerateModels(tree);
-
-            Observer observer = new Observer(collection);
-            var score = calculator.GetScore("OBSERVER", observer.GetResult());
-            Assert.Equal(100, score);
-        }
-
-        [Fact]
-        public void Test_Observer_NoIObserverParent()
-        {
-            Requirements r = new Requirements();
-            ScoreCalculator calculator = new ScoreCalculator(r.GetRequirements());
-            var tree = NoInterfaces();
-            var collection = Walker.GenerateModels(tree);
-
-            Observer observer = new Observer(collection);
-            var score = calculator.GetScore("OBSERVER", observer.GetResult());
-            Assert.NotEqual(100, score);
-        }
-
-        [Fact]
-        public void Test_Observer_HasNoObserverInterface()
-        {
-            Requirements r = new Requirements();
-            ScoreCalculator calculator = new ScoreCalculator(r.GetRequirements());
-            var tree = NoInterfaces();
-            var collection = Walker.GenerateModels(tree);
-
-            Observer observer = new Observer(collection);
-            var score = calculator.GetScore("OBSERVER", observer.GetResult());
-            Assert.NotEqual(100, score);
-        }
-
-        [Fact]
-        public void Test_Observer_SubjectHasNoObserverList()
-        {
-            Requirements r = new Requirements();
-            ScoreCalculator calculator = new ScoreCalculator(r.GetRequirements());
-            var tree = NoObserverList();
-            var collection = Walker.GenerateModels(tree);
-
-            Observer observer = new Observer(collection);
-            var score = calculator.GetScore("OBSERVER", observer.GetResult());
-            Assert.NotEqual(100, score);
-        }
     }
 }
